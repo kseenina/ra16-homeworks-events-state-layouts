77bf4893d8988fc39a2da43614f8a9ae4d3d7a56
Ksenia Timokhina <kseenina@gmail.com>
2026-09-11 20:27:58 +0300

    Component ShopCard realized

--- src/ShopCard.jsx
+++ src/ShopCard.jsx
@@ -1,3 +1,14 @@
 export default function ShopCard({product}) {
-    
+    const { name, price, color, img } = product;
+    return (
+        <div className="shop-card">
+            <p className="name">{name}</p>
+            <p className="color">{color}</p>
+            <img src={img} alt={name + ' ' + color} className="img" />
+            <div className="price-block">
+                <p className="price">${price}</p>
+                <button className="add-to-cart">add to cart</button>
+            </div>
+        </div>
+    );
 }
