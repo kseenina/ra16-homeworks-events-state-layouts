a7c6020b78d081ae9f32ba443f03fd6d62c8cd6a
Ksenia Timokhina <kseenina@gmail.com>
2026-09-11 20:18:35 +0300

    Component CardView realized

--- src/CardsView.jsx
+++ src/CardsView.jsx
@@ -1,5 +1,11 @@
 import ShopCard from "./ShopCard";
 
-export default function CardsView(cards) {
-
+export default function CardsView({cards}) {
+    return (
+        <div className="cards-view">
+            {cards.map((product, index) => (
+                <ShopCard key={index} product={product} />
+            ))}
+        </div>
+    );
 }
--- src/ShopCard.jsx
+++ src/ShopCard.jsx
@@ -1,3 +1,3 @@
-export default function ShopCard() {
+export default function ShopCard({product}) {
     
 }
