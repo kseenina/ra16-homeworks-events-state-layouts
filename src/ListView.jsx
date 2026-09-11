18e20af0b8c42bcbbce46374d7f1d6ff96d041c3
Ksenia Timokhina <kseenina@gmail.com>
2026-09-11 20:37:10 +0300

    Component ListView realized

--- src/ListView.jsx
+++ src/ListView.jsx
@@ -1,3 +1,11 @@
-export default function ListView() {
-    
+import ShopItem from "./ShopItem";
+
+export default function ListView({items}) {
+    return(
+        <div className="list-view">
+            {items.map((product, index) => (
+                <ShopItem key={index} product={product} />
+            ))}
+        </div>
+    );
 }
